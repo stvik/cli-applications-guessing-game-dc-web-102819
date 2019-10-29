1e0989ebafdd74c1018493bfc0d0aaa9ce30c758
Sara Tarnvik <stvik2@gmail.com>
2019-10-29 02:27:04 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,7 @@ def run_guessing_game()
   guess = gets.chomp
   if guess == "exit"
     puts "Goodbye!"
-  elsif number != guess
+  elsif guess != number
     puts "Sorry! The computer guessed #{number}."
   else
     puts "You guessed the correct number!"
